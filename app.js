2f32df6c279fd626f5f6ea8f5385e1db5bf158bf
Wlainer Ribeiro <wlainer@gmail.com>
2014-09-18 16:43:05 -0300

    Initial branch

--- app.js
+++ app.js
@@ -1,17 +1,48 @@
+var http = require('http');
 var express = require('express');
-var path = require('path');
+var fs = require('fs');
 var app = express();
-var routes = require('./routes')(app);
 
-app.use(function(req, res, next) {
-    console.log('Request from: ', req.ip);
-    next();
-});
+var iniparser = require('iniparser');
+var config = iniparser.parseSync('./config.ini');
 
-app.use(express.static(path.join(__dirname, '/public')));
-app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'jade');
+app.set('views', './views');
 
-var server = app.listen(3000, function() {
-    console.log('Listening on port %d', server.address().port);
+app.use(express.logger({
+    format: 'tiny',
+    stream: fs.createWriteStream('app.log', {"flags": "w"})
+}));
+app.use(express.static('./public'));
+app.use(express.responseTime());
+app.use(app.router);
+app.use(express.errorHandler());
+
+//app.use(express.logger('dev'));
+
+app.get('/',
+
+    function(req, res, next) {
+        res.set('X-One', 'hey!');
+        next();
+    },
+
+    function(req, res, next) {
+        res.set('X-Two', 'ho!');
+        next();
+    },
+
+    function(req, res) {
+        res.send('Let´s go!');
+    }
+);
+
+/**
+app.get('/', function(req, res) {
+    res.render('index', {title: config.title, message:config.message});
+});
+*/
+
+http.createServer(app).listen(config.port, function() {
+    console.log('App started on port ' + config.port);
 });
